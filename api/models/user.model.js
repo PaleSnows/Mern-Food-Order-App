9d7f1517e7567b9b67bf49ab7fc9452f02cf9391
Renzaki <renzakithakuri@gmail.com>
2024-04-07 14:18:13 +0545

    creating schema for user

---
+++ api/models/user.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      requiired: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      requiired: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      requiired: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
