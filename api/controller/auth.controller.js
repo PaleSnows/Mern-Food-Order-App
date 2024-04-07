c0dc5f5fb8a90768a03f6774b44e7d01a74a25a9
Renzaki <renzakithakuri@gmail.com>
2024-04-07 16:06:22 +0545

    create api for signin

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,16 +1,35 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import { errorHandler } from "../utility/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   try {
     const { username, email, password } = await req.body;
     const hashedPassword = bcrypt.hashSync(password, 10);
-   const newUser =  new User({ username, email, hashedPassword });
-   await newUser.save()
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
     res.status(201).json({ message: "User created successfully!!!" });
   } catch (error) {
-    next(errorHandler(500,"error from server"));
+    next(errorHandler(500, "error from server"));
   }
 };
 
-export const signin = async (req, res) => {};
+export const signin = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User not found"));
+    }
+    const validPassword = bcrypt.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(401, "Invalid credentials"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, rest } = validUser._doc;
+    res.cookie("access_token", token,{httpOnly:true}).status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- api/models/user.model.js
+++ api/models/user.model.js
@@ -4,17 +4,17 @@ const userSchema = new mongoose.Schema(
   {
     username: {
       type: String,
-      requiired: true,
+      required: true,
       unique: true,
     },
     email: {
       type: String,
-      requiired: true,
+      required: true,
       unique: true,
     },
     password: {
       type: String,
-      requiired: true,
+      required: true,
     },
   },
   { timestamps: true }
